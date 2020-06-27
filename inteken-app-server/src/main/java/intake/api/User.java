c209e63e5690041e847d1634e4c4091dc5dfaa6b
Okke Harsta <oharsta@zilverline.com>
2020-06-27 08:59:18 +0200

    Imports hygiene

--- inteken-app-server/src/main/java/intake/api/User.java
+++ inteken-app-server/src/main/java/intake/api/User.java
@@ -1,6 +1,5 @@
 package intake.api;
 
-import intake.repository.CatalogRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
