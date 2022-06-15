2c8ce89fc17eb26475fb63c9f5ee21aa694081c2
Okke Harsta <oharsta@zilverline.com>
2022-06-15 12:06:34 +0200

    Test internal endpoints

--- student-mobility-broker-server/src/test/java/broker/BrokerApplicationTest.java
+++ student-mobility-broker-server/src/test/java/broker/BrokerApplicationTest.java
@@ -1,11 +1,30 @@
 package broker;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class BrokerApplicationTest {
 
     @Test
     public void main() {
         BrokerApplication.main(new String[]{"--server.port=8066"});
+        RestAssured.port = 8066;
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/internal/health")
+                .then()
+                .body("status", equalTo("UP"));
+
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/internal/info")
+                .then()
+                .body("build.artifact", equalTo("student-mobility-broker-server"));
     }
 }
