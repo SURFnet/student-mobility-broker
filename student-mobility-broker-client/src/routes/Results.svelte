3dcd56a96e11cb4500d2a737fe4a81db0999ab2b
Okke Harsta <oharsta@zilverline.com>
2022-06-20 11:03:45 +0200

    Small improvement in playground

--- student-mobility-broker-client/src/routes/Results.svelte
+++ student-mobility-broker-client/src/routes/Results.svelte
@@ -3,6 +3,7 @@
     import Select from 'svelte-select';
     import {newAssociationPlayground, updateAssociationPlayground} from "../api";
     import Button from "../components/Button.svelte";
+    import Loading from "../components/Loading.svelte";
 
     export let institutions = [];
 
@@ -19,8 +20,10 @@
     let remoteState = remoteStates[0];
 
     let finished = false;
+    let loading = false;
 
     const newAssociation = () => {
+        loading = true;
         const results = {
             role: "student",
             remoteState: remoteState.label
@@ -28,11 +31,13 @@
         newAssociationPlayground($offering.correlationID, results).then(res => {
             finished = true;
             $offering.associationId = res.associationId;
+            loading = false;
             setTimeout(() => finished = false, 7500 * 2);
         });
     }
 
     const updateAssociation = () => {
+        loading = true;
         const results = {
             remoteState: "associated",
             result: {
@@ -42,6 +47,7 @@
         };
         updateAssociationPlayground($offering.correlationID, results, $offering.associationId).then(() => {
             finished = true;
+            loading = false;
             setTimeout(() => finished = false, 7500 * 2);
         });
     }
@@ -115,75 +121,84 @@
 
 </style>
 <div class="page">
-    <h4>Mock an enrollment result</h4>
-    {#if !$offering.correlationID || $offering.correlationID === "1"}
-        <p class="note">In order to mimic the POST results back to the intake-ontvanger-generiek you need to first
-            login and
-            successfully enroll.</p>
+    {#if loading}
+        <Loading/>
     {:else}
-        {#if finished}
-            <p>Check your mailbox for a association email.</p>
-        {/if}
-        <div class="institution">
-            <div class="institution-detail">
-                <p>Home institution</p>
-                <span class="info">Institution where the user has registered</span>
-                <Select items={institutions}
+        <h4>Mock an enrollment result</h4>
+        {#if !$offering.correlationID || $offering.correlationID === "1"}
+            <p class="note">In order to mimic the POST results back to the intake-ontvanger-generiek you need to first
+                login and
+                successfully enroll.</p>
+        {:else}
+            {#if finished}
+                <p>Check your mailbox for a association email.</p>
+            {/if}
+            <div class="institution">
+                <div class="institution-detail">
+                    <p>Home institution</p>
+                    <span class="info">Institution where the user has registered</span>
+                    <Select items={institutions}
+                            isSearchable={false}
+                            showIndicator={true}
+                            isClearable={false}
+                            selectedValue={$offering.homeInstitution}
+                            isDisabled={true}/>
+                </div>
+                <img src={$offering.homeInstitution.logoURI} alt=""/>
+            </div>
+            <div class="institution">
+                <div class="institution-detail">
+                    <p>Guest institution</p>
+                    <span class="info">Institution where the user has enrolled for a  course</span>
+                    <Select items={institutions}
+                            isSearchable={false}
+                            showIndicator={true}
+                            isClearable={false}
+                            selectedValue={$offering.guestInstitution}
+                            isDisabled={true}/>
+                </div>
+                <img src={$offering.guestInstitution.logoURI} alt=""/>
+            </div>
+            {#if !$offering.associationId}
+                <p>Association</p>
+                <span class="info">The state of this association for the institution performing the request.</span>
+                <Select items={remoteStates}
                         isSearchable={false}
                         showIndicator={true}
                         isClearable={false}
-                        selectedValue={$offering.homeInstitution}
-                        isDisabled={true}/>
-            </div>
-            <img src={$offering.homeInstitution.logoURI} alt=""/>
-        </div>
-        <div class="institution">
-            <div class="institution-detail">
-                <p>Guest institution</p>
-                <span class="info">Institution where the user has enrolled for a  course</span>
-                <Select items={institutions}
+                        selectedValue={remoteState}
+                        on:select={handleSelectRemoteState}/>
+            {/if}
+            {#if $offering.associationId}
+                <p>Results</p>
+                <span class="info">It's either very good or very bad</span>
+                <Select items={responses}
                         isSearchable={false}
                         showIndicator={true}
                         isClearable={false}
-                        selectedValue={$offering.guestInstitution}
-                        isDisabled={true}/>
+                        selectedValue={response}
+                        on:select={handleSelectResult}/>
+            {/if}
+            {#if $offering.correlationID}
+                <p>Correlation ID used to mimic the POST results back to intake-ontvanger-generiek and subsequently the
+                    home
+                    institution:</p>
+                <span>{$offering.correlationID}</span>
+            {/if}
+            {#if $offering.associationId}
+                <p>AssociationId ID returned from the home institution:</p>
+                <span>{$offering.associationId}</span>
+            {/if}
+            <div class="actions">
+                <Button onClick={updateAssociation}
+                        active={true}
+                        disabled={!$offering.associationId || loading}
+                        label="Update association"/>
+                <Button onClick={newAssociation}
+                        active={true}
+                        disabled={$offering.associationId || loading}
+                        label="New association"/>
             </div>
-            <img src={$offering.guestInstitution.logoURI} alt=""/>
-        </div>
-        {#if !$offering.associationId}
-            <p>Association</p>
-            <span class="info">The state of this association for the institution performing the request.</span>
-            <Select items={remoteStates}
-                    isSearchable={false}
-                    showIndicator={true}
-                    isClearable={false}
-                    selectedValue={remoteState}
-                    on:select={handleSelectRemoteState}/>
         {/if}
-        {#if $offering.associationId}
-            <p>Results</p>
-            <span class="info">It's either very good or very bad</span>
-            <Select items={responses}
-                    isSearchable={false}
-                    showIndicator={true}
-                    isClearable={false}
-                    selectedValue={response}
-                    on:select={handleSelectResult}/>
-        {/if}
-        {#if $offering.correlationID}
-            <p>Correlation ID used to mimic the POST results back to intake-ontvanger-generiek and subsequently the home
-                institution:</p>
-            <span>{$offering.correlationID}</span>
-        {/if}
-        {#if $offering.associationId}
-            <p>AssociationId ID returned from the home institution:</p>
-            <span>{$offering.associationId}</span>
-        {/if}
-        <div class="actions">
-            <Button onClick={updateAssociation} active={true} disabled={!$offering.associationId}
-                    label="Update association"/>
-            <Button onClick={newAssociation} active={true} disabled={$offering.associationId} label="New association"/>
-        </div>
     {/if}
-
 </div>
